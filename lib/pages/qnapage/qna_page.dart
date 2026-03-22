629c2f18e803dcc59d5b6c2bbfdadd99a3a6d516
Razvan-arh <zcabrar@ucl.ac.uk>
2026-03-22 21:48:42 +0200

    finished qna page

--- lib/pages/qnapage/qna_page.dart
+++ lib/pages/qnapage/qna_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
-// The barcode scanner doesn't work on Windows/IOS/Linux
 class BarcodeScannerPage extends StatelessWidget {
   const BarcodeScannerPage({super.key});
 
